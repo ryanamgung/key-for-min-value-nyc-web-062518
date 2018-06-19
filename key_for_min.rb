9a518c3dddb7bad9bc8a624f71dd5156c9ca266f
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-19 04:29:43 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest = 1000000000
+  lowest = name_hash90
   key = ""
   if name_hash.length == 0
     return 
